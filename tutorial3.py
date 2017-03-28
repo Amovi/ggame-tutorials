cc29efcfb72d8d4f0180b0fb6e1928c97a5d5081
Amovi <anthony.movizzo@outlook.com>
2017-03-28 14:54:17 -0400

    Updated from Brython Server: 3/28/2017 2:54:17 PM

--- tutorial3.py
+++ tutorial3.py
@@ -10,7 +10,26 @@ def step():
             ball.x -= ball.dir
             reverse(ball)
 myapp = App()
+
+# Set up event handlers for the app
+myapp.listenKeyEvent('keydown', 'space', spaceKey)
+myapp.listenKeyEvent('keydown', 'r', reverseKey)
+myapp.listenMouseEvent('click', mouseClick)
+
 myapp.run(step)
+
+# Handle the space key
+def spaceKey(event):
+    ball.go = not ball.go
+
+# Handle the "reverse" key
+def reverseKey(event):
+    reverse(ball)
+
+# Handle the mouse click
+def mouseClick(event):
+    ball.x = event.x
+    ball.y = event.y
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
@@ -19,11 +38,11 @@ black = Color(0, 1)
 noline = LineStyle(0, black)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
 bg = Sprite(bg_asset, (0,0))
-# A ball! This is already in the ggame-tutorials repository
+
 ball_asset = ImageAsset("images/4236-0-1449112382.png")
 ball = Sprite(ball_asset, (0, 0))
-# Original image is too big. Scale it to 1/10 its original size
+
 ball.scale = 0.3
-# custom attributes
+
 ball.dir = 1
 ball.go = True
